805b8b159035a4838815083223806e3c4c2ec446
Eduardo Grajales <edgravill@gmail.com>
2018-03-27 15:26:15 -0600

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'rb-navbar',
+    libraryTarget: 'umd',
+    pathinfo: true,
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['env', 'react', 'flow', 'stage-2'],
+      },
+    }, {
+      test: /\.css$/,
+      use: [
+        { loader: 'style-loader' },
+        { loader: 'css-loader' },
+      ],
+    }, {
+      test: /\.(ttf|eot|woff|woff2|svg)$/,
+      loader: 'file-loader',
+      options: {
+        name: 'fonts/[name].[ext]',
+      },
+    }],
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx', '.css'],
+  },
+  devtool: 'source-map',
+  target: 'web',
+};
