d062563277e0703c850830dd8be27eb0c1ed2148
Eduardo Grajales <edgravill@gmail.com>
2018-03-27 14:39:20 -0600

    migration

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: 'airbnb',
+  parser: 'babel-eslint',
+  plugins: ['flowtype'],
+  globals: {
+    document: true,
+    window: true,
+    HTMLAnchorElement: true,
+    HTMLDivElement: true,
+    HTMLElement: true,
+    HTMLInputElement: true,
+    React$Element: true,
+  }
+};
